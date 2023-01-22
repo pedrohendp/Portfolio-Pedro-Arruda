72313de0a5afc785a285a795163d25434d33e24d
Pedro Arruda <pedrohendp@gmail.com>
2023-01-22 13:00:32 -0300

    01

--- Portfolio-v0.1/assets/js/main.js
+++ Portfolio-v0.1/assets/js/main.js
@@ -22,6 +22,15 @@ if(navClose){
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 
 /*==================== ACCORDION SKILLS ====================*/
 
